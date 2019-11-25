Fixed computation of the node volume

IForest/IForest.py
@@ -61,8 +61,16 @@ class IForest:
             wait(futures)
             score = np.zeros(X.shape[0])
             for future in futures:
-                score += self._compute_score(*future.result())
+                leaf_size = future.result()[:, 0]
+                log_scaled_ratio = future.result()[:, 1]
+                score += self._compute_score(leaf_size, log_scaled_ratio)
         return score
 
-    def _compute_score(self, leaf_size, log_scale_ratio):
-        res = np.exp(np.log(leaf_size) - log_scale_ratio - np.log(self.max_samples))
+    def _compute_score(self, leaf_size: np.ndarray, log_scaled_ratio: np.ndarray):
+        '''
+        Computes the density associated to each samples based on the node in which they fall.
+        :param leaf_size: ndarray (n_samples, ), the number of samples contained in the leaf where sample falls.
+        :param log_scale_ratio: ndarray (n_samples, ), the leaf log-scaled volume ratio where sample falls.
+        :return:
+        '''
+        return np.exp(np.log(leaf_size) - log_scaled_ratio - np.log(self.max_samples))

IForest/ITree/INode.py
@@ -12,12 +12,18 @@ class INode:
     splitAtt: int
     splitValue: int
     logScaledRatio: float
+    parent: 'INode'
 
     # Fit the node and eventually create its children
-    def fit(self, X: np.ndarray, e: int, l: int, fw: np.ndarray, logScaleRatio=0.):
+    def fit(self, X: np.ndarray, e: int, l: int, fw: np.ndarray, parent: 'INode' = None, prev_random_value: float = 1.):
         self.leaf = True
         self.size = X.shape[0]
         self.boundaries = np.apply_along_axis(lambda x: (x.min(), x.max()), axis=0, arr=X).T
+        self.parent = parent
+        if parent is not None:
+            self.logScaledRatio = parent.logScaledRatio + np.log(prev_random_value)
+        else:
+            self.logScaledRatio = 0.
         # Create node children if the conditions are satisfied
         if e < l and X.shape[0] > 1 and not np.isclose(X, X[0]).all():
             self.leaf = False
@@ -25,19 +31,20 @@ class INode:
             indices = np.asarray(range(X.shape[1]))[np.apply_along_axis(lambda x: not np.isclose(x, x[0]).all(),
                                                                         axis=1, arr=self.boundaries)]
             # Pick up randomly a split attribute and value among the valid ones
-            self.splitAtt = np.random.choice(indices, p=fw[indices]/sum(fw[indices]))
+            self.splitAtt = np.random.choice(indices, p=fw[indices] / sum(fw[indices]))
             self.splitValue = np.random.uniform(self.boundaries[self.splitAtt][0],
                                                 self.boundaries[self.splitAtt][1])
-
-            self.logScaleRatio = logScaleRatio + max(np.log(self.splitValue - self.boundaries[0]), 0)
-
+            curr_random_value = self.splitValue - self.boundaries[self.splitAtt][0]
+            curr_random_value /=  (self.boundaries[self.splitAtt][1] - self.boundaries[self.splitAtt][0])
             # Build child nodes using multithreading
             futures = []
             with ThreadPoolExecutor(max_workers=2) as executor:
                 futures.append(executor.submit(INode().fit,
-                                               X[X[:, self.splitAtt] <= self.splitValue], e + 1, l, fw, self.logScaleRatio))
+                                               X[X[:, self.splitAtt] <= self.splitValue], e + 1, l, fw,
+                                               self, curr_random_value))
                 futures.append(executor.submit(INode().fit,
-                                               X[X[:, self.splitAtt] > self.splitValue], e + 1, l, fw, self.logScaleRatio))
+                                               X[X[:, self.splitAtt] > self.splitValue], e + 1, l, fw,
+                                               self, 1. - curr_random_value))
             wait(futures)
             self.left = futures[0].result()
             self.right = futures[1].result()
@@ -52,9 +59,9 @@ class INode:
         else:  # x[self.splitAtt] > self.splitValue
             return self.right.profile(x, e + 1)
 
-    def score(self, x:np.ndarray):
+    def score(self, x: np.ndarray):
         if self.leaf:
-             return self.size, self.logScaleRatio
+            return self.size, self.logScaledRatio
         elif x[self.splitAtt] <= self.splitValue:
             return self.left.score(x)
         else:

IForest/ITree/ITree.py
@@ -33,11 +33,11 @@ class ITree:
     def score(self, X: np.ndarray):
         '''
         :param X: ndarray, shape(n_samples, n_dimensions)
-        :return: ndarray, shape(n_samples, n_dimensions), the first column is the leaf cardinality which contains the
+        :return: ndarray, shape(n_samples, 2), the first column is the leaf cardinality which contains the
         sample x, the second column is the log ratio of the leaf volume with features volume
         '''
         X = X[:, self.features_indices]
-        scores = np.zeros((X.shape[0], self.feature_indices.shape))
+        scores = np.zeros((X.shape[0], 2))
         for i, x in enumerate(X):
             scores[i] = self.root.score(x)
         return scores